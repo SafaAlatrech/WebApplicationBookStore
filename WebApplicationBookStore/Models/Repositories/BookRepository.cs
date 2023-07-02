688c4b76d0ce8de1a99ec47784f9cd481f980a77
SafaAlatrech <70481654+SafaAlatrech@users.noreply.github.com>
2023-07-02 12:24:10 +0200

    AddIBookStoreRepository (#3)
    * AddIBookStoreRepository
    
    * AddBookRepository
    
    * Add Author Repository

---
+++ WebApplicationBookStore/Models/Repositories/BookRepository.cs
@@ -0,0 +1,56 @@
+﻿namespace WebApplicationBookStore.Models.Repositories
+{
+    public class BookRepository : IBookStoreRepository<Book>
+    {
+        List<Book> books;
+
+        public BookRepository()
+        {
+            books = new List<Book>()
+            {
+                new Book
+                {
+                    Id = 1,Title="C# Programming",Description="No description"
+                },
+                new Book
+                {
+                    Id = 2,Title="Java Programming",Description="No description"
+                },
+                new Book
+                {
+                    Id = 3,Title="PHP Programming",Description="No description"
+                }
+            };
+        }
+
+        public void Add(Book entity)
+        {
+            books.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var book = Find(id);
+            books.Remove(book);
+        }
+
+        public Book Find(int id)
+        {
+            var book = books.SingleOrDefault(b => b.Id == id);
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+           return books;
+        }
+
+        public void Update(int id, Book newBook)
+        {
+            var book = Find(id);
+            book.Title = newBook.Title;
+            book.Author = newBook.Author;
+            book.Description = newBook.Description;
+        }
+    }
+}
---
+++ WebApplicationBookStore/Models/Repositories/IBookStoreRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplicationBookStore.Models.Repositories
+{
+    public interface IBookStoreRepository<TEntity>
+    {
+        IList<TEntity> List();
+        TEntity Find(int id); 
+        void Delete(int id);
+        void Add(TEntity entity);
+        void Update(int id,TEntity entity);
+    }
+}
