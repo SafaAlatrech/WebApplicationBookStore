74b6d44d81ef072b335891855861e127fe3c06b1
SafaAlatrech <alatrechsafa@gmail.com>
2023-07-08 14:42:00 +0200

    Add Book Controller

---
+++ WebApplicationBookStore/Controllers/BookController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationBookStore.Models;
+using WebApplicationBookStore.Models.Repositories;
+
+namespace WebApplicationBookStore.Controllers
+{
+    public class BookController : Controller 
+    { 
+        private readonly IBookStoreRepository<Book>? bookRepository;
+
+        public BookController(IBookStoreRepository<Book> bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+        // GET: BookController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: BookController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BookController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BookController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
