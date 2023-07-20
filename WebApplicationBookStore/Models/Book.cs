8019d09b5dbcd9633ab60583cf46e8863abdec99
SafaAlatrech <70481654+SafaAlatrech@users.noreply.github.com>
2023-07-20 17:07:00 +0200

    Upload Image  (#13)
    * Upload Image Part I
    
    * Upload Image Part II

--- WebApplicationBookStore/Models/Book.cs
+++ WebApplicationBookStore/Models/Book.cs
@@ -6,6 +6,7 @@
 
         public string? Title { get; set; }
         public string? Description  { get; set; } 
+        public string? ImageURL  { get; set; } 
         public Author? Author { get; set; }
     }
 }
