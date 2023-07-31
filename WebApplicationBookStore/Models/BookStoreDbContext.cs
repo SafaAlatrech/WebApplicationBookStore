d37a48258d999468ebb7bfa232a844a55188951f
SafaAlatrech <70481654+SafaAlatrech@users.noreply.github.com>
2023-07-31 18:28:34 +0200

    AddDbContextUsingEFC (#15)

---
+++ WebApplicationBookStore/Models/BookStoreDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationBookStore.Models
+{
+    public class BookStoreDbContext : DbContext
+    {
+        //public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options) : base(options)
+        //{
+
+        //}
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS; Database=test; Integrated security=true");
+        }
+
+    }
+}
--- WebApplicationBookStore/Program.cs
+++ WebApplicationBookStore/Program.cs
@@ -1,15 +1,21 @@
 using WebApplicationBookStore.Models;
 using WebApplicationBookStore.Models.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddMvc();
-builder.Services.AddSingleton<IBookStoreRepository<Author>,AuthorRepository>();
-builder.Services.AddSingleton<IBookStoreRepository<Book>,BookRepository>();
+builder.Services.AddSingleton<IBookStoreRepository<Author>, AuthorRepository>();
+builder.Services.AddSingleton<IBookStoreRepository<Book>, BookRepository>();
 builder.Services.AddControllers();
-builder.Services.AddRazorPages();
+//builder.Services.AddDbContext<BookStoreDbContext>(options =>
+//{
+//    //options.UseSqlServer(IConfiguration.GetConnecionString("SqlConnexion"));
+//});
 
 
 var app = builder.Build();
@@ -20,7 +26,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-  
+
 
 }
 
