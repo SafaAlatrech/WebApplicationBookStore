dc88d5b4f1435110a4f475e0989a393e43895204
SafaAlatrech <70481654+SafaAlatrech@users.noreply.github.com>
2023-08-01 20:45:46 +0200

    EntityFrameworkCoreRepository (#16)

---
+++ WebApplicationBookStore/Models/Repositories/AuthorDbRepository.cs
@@ -0,0 +1,47 @@
+﻿namespace WebApplicationBookStore.Models.Repositories
+{
+    public class AuthorDbRepository : IBookStoreRepository<Author>
+    {
+        BookStoreDbContext db;
+
+        public AuthorDbRepository(BookStoreDbContext _db)
+        {
+           this.db = _db;
+        }
+        public void Add(Author entity)
+        {
+            entity.Id = db.Authors.Max(b => b.Id) + 1;
+            db.Authors.Add(entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var author = Find(id);
+            db.Authors.Remove(author);
+            db.SaveChanges();
+        }
+
+        public Author Find(int id)
+        {
+            var author = db.Authors.SingleOrDefault(b => b.Id == id);
+            return author;
+        }
+
+        public IList<Author> List()
+        {
+            return db.Authors.ToList();
+        }
+
+        public void Update(int id, Author newAuthor)
+        {
+            db.Authors.Update(newAuthor);
+            db.SaveChanges();
+            //var author = Find(id);
+            //author.Id = newAuthor.Id;
+            //author.FullName = newAuthor.FullName;
+
+        }
+    }
+
+}
---
+++ WebApplicationBookStore/Models/Repositories/BookDbRepositorycs.cs
@@ -0,0 +1,50 @@
+﻿namespace WebApplicationBookStore.Models.Repositories
+{
+    public class BookDbRepositorycs : IBookStoreRepository<Book>
+    {
+        BookStoreDbContext db;
+
+        public BookDbRepositorycs(BookStoreDbContext _db)
+        {
+            this.db = _db;
+
+        }
+
+        public void Add(Book entity)
+        {
+            entity.Id = db.Books.Max(b => b.Id) + 1;
+            db.Books.Add(entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var book = Find(id);
+            db.Books.Remove(book);
+            db.SaveChanges();
+        }
+
+        public Book Find(int id)
+        {
+            var book = db.Books.SingleOrDefault(b => b.Id == id);
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+            return db.Books.ToList();
+        }
+
+        public void Update(int id, Book newBook)
+        {
+            db.Books.Update(newBook);
+            db.SaveChanges();
+            //var book = Find(id);
+            //book.Title = newBook.Title;
+            //book.Author = newBook.Author;
+            //book.Description = newBook.Description;
+            //book.ImageURL = newBook.ImageURL;
+        }
+    }
+
+}
